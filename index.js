7d9de58a50cd0d9f192e3b176ce4392c3c2b3112
Robert Kelley <robertkelley1987@gmail.com>
2022-07-03 03:49:42 -0400

    Added catch-async function to catch errors in async routes

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const List = require('./models/List');
 const ExpressError = require('./util/express-error');
 const isLoggedIn = require('./middleware');
 const bcrypt = require('bcrypt');
+const catchAsync = require('./util/catch-async');
 
 mongoose.connect(mongoUrl, { useNewUrlParser: true });
 app.use(session);
@@ -23,7 +24,7 @@ app.get('/sessions', isLoggedIn, (req, res) => {
 });
 
 // SIGN UP, LOG IN AND LOGOUT ROUTES 
-app.post('/signup', async (req, res) => {
+app.post('/signup', catchAsync(async (req, res) => {
     const { email, password } = req.body;
     const foundUser = await User.findOne({ email: email });
     if(!foundUser) {
@@ -34,11 +35,11 @@ app.post('/signup', async (req, res) => {
         req.session.userId = _id;
         res.status(200).send({ userId: _id});
     } else {
-        throw new ExpressError(400, 'This user already exists in the system');
+        throw new ExpressError(404, 'This user already exists in the system');
     }
-});
+}));
 
-app.post('/login', async (req, res) => {
+app.post('/login', catchAsync(async (req, res) => {
     const { email, password } = req.body;
     const foundUser = await User.findOne({ email: email });
     if(!foundUser) {
@@ -50,15 +51,15 @@ app.post('/login', async (req, res) => {
     }
     req.session.userId = foundUser._id;
     res.status(200).send({ userId: foundUser._id });
-});
+}));
 
-app.post('/logout', async (req, res) => {
+app.post('/logout', catchAsync(async (req, res) => {
     req.session.userId = null;
     res.status(200).send({ userId: null, message:'OK' });
-});
+}));
 
 // USER -- GET USER DATA (favs and lists)
-app.get('/users/:userId', async (req, res) => {
+app.get('/users/:userId', catchAsync(async (req, res) => {
     const { userId } = req.params;
     const foundUser = await User.findById(userId).populate('lists');
     console.log("LISTS:");
@@ -68,10 +69,10 @@ app.get('/users/:userId', async (req, res) => {
         throw new ExpressError(404, 'The user with the user id provided could not be found');
     }
     res.status(200).send({ favorites: foundUser.favorites, lists: foundUser.lists}) 
-});
+}));
 
 // FAVORITES -- TOGGLE AND GET ROUTES
-app.post('/users/:userId/favorites', async (req, res) => {
+app.post('/users/:userId/favorites', catchAsync(async (req, res) => {
     const { userId } = req.params, { recipeId } = req.body;
     const foundUser = await User.findById(userId);
     if(!foundUser) {
@@ -85,19 +86,19 @@ app.post('/users/:userId/favorites', async (req, res) => {
     }
     await foundUser.save();
     res.status(200).send({ favorites: foundUser.favorites });
-});
+}));
 
-app.get('/users/:userId/favorites', async (req, res) => {
+app.get('/users/:userId/favorites', catchAsync(async (req, res) => {
     const { userId } = req.params;
     const foundUser = await User.findById(userId);
     if(!foundUser) {
         throw new ExpressError(404, 'The user with the user id provided could not be found');
     } 
     res.status(200).send({ favorites: foundUser.favorites });
-});
+}));
 
 // LIST ROUTES
-app.post('/users/:userId/lists', async (req, res) => {
+app.post('/users/:userId/lists', catchAsync(async (req, res) => {
     const { userId } = req.params, { name } = req.body;
     const foundUser = await User.findById(userId);
     if(!foundUser) {
@@ -110,33 +111,42 @@ app.post('/users/:userId/lists', async (req, res) => {
     foundUser.lists.push(newList);
     await foundUser.save();
     res.status(200).send({ listId: newList._id });
-});
+}));
 
-app.get('/lists/:listId', async (req, res) => {
+app.get('/lists/:listId', catchAsync(async (req, res) => {
     const { listId } = req.params;
     const foundList = await List.findById(listId);
     if(!foundList) {
-        throw new ExpressError(400, 'The list with this id could not be found');
+        throw new ExpressError(404, 'The list with this id could not be found');
     }
     res.status(200).send({ list: foundList });
-});
+}));
 
-app.post('/lists/:listId/recipes', async (req, res) => {
+app.post('/lists/:listId/recipes', catchAsync(async (req, res) => {
     const { listId } = req.params, { recipeId } = req.body;
     const foundList = await List.findById(listId);
     if(!foundList) {
-        throw new ExpressError(400, 'The list with this id could not be found');
+        throw new ExpressError(404, 'The list with this id could not be found');
     }
     foundList.recipes.push(recipeId)
     await foundList.save();
     res.status(200).send({ list: foundList });
-});
+}));
 
 // ERROR HANDLING
 app.use((err, req, res, next) => {
-    if(err.statusCode === 401 || err.statusCode === 404) {
-        return res.send({ userId: null, errorCode: err.statusCode, errorMessage: err.message });
+    if(err.statusCode === 401) {
+        return res.send({ userId: null, err: err });
     }
+
+    if(err.statusCode === 400) {
+        return res.send({ userId: null, err: err });
+    }
+
+    if(err.statusCode === 404) {
+        return res.send({ err: err });
+    }
+
 })
 
 app.listen(3001, () => console.log('The server listens...'));
